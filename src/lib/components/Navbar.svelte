c6012f578c93d17be56d998bce8961124e8596a9
sxht <31171951+sxht@users.noreply.github.com>
2025-06-07 16:33:36 -0700

    Styling fixes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,40 +1,39 @@
 <script lang="ts">
-    import { fly } from 'svelte/transition';
-    import { scrollY } from 'svelte/reactivity/window';
+	import { fly } from 'svelte/transition';
+	import { scrollY } from 'svelte/reactivity/window';
 
-    let open = $state(false);
+	let open = $state(false);
 </script>
 
 <!-- you can ignore the scrollY undefined error -->
-<nav class={`sticky bg-secondary top-0 left-0 w-full z-50 transition-all duration-300 
-${scrollY.current > 50 ? 'opacity-80 shadow-lg' : 'opacity-100'}
-`}>
-  <div class="flex items-center justify-between w-full px-3">
-    <img class="w-[20vw] h-auto" src="/images/logo.png" alt="Logo" />
-    <div class="absolute left-1/2 -translate-x-1/2">
-      <img class="w-[50vw] h-auto" src="/images/logoText.png" alt="Logo Text" />
-    </div>
-    <button onclick={() => (open = !open)} aria-label="open navigation menu">
-      <img src="/icons/Menu.png" alt="" class="w-[10vw] h-auto" />
-    </button>
-  </div>
+<nav class="sticky top-0 left-0 z-50 w-full">
+	<div
+		class={`bg-secondary w-full transition-all duration-300 
+    ${scrollY.current > 50 ? 'opacity-80 shadow-lg' : 'opacity-100'}`}
+	>
+		<div class="flex w-full items-center justify-between px-3">
+			<img class="h-auto w-[20vw]" src="/images/logo.png" alt="Logo" />
+			<div class="absolute left-1/2 -translate-x-1/2">
+				<img class="h-auto w-[50vw]" src="/images/logoText.png" alt="Logo Text" />
+			</div>
+			<button onclick={() => (open = !open)} aria-label="open navigation menu">
+				<img src="/icons/Menu.png" alt="" class="h-auto w-[10vw]" />
+			</button>
+		</div>
+	</div>
 
-  {#if open}
-    <div class="fixed inset-0 z-40 flex" transition:fly={{ x: 100, duration: 500 }}>
-      <button
-        class="w-[30%] h-[100vh]"
-        onclick={() => (open = !open)}
-        aria-label="close navigation menu"
-      ></button>
-      <div
-        class="flex flex-col items-center pt-5 gap-3 bg-gray-500/90 w-[70%] h-[100vh]"
-      >
-        <a href="/" class="text-secondary text-2xl" onclick={() => (open = false)}>Home</a>
-        <a href="/menu" class="text-secondary text-2xl" onclick={() => (open = false)}>Menu</a>
-        <a href="/about" class="text-secondary text-2xl" onclick={() => (open = false)}>About</a>
-      </div>
-    </div>
-  {/if}
-
+	{#if open}
+		<div class="fixed inset-0 z-40 flex" transition:fly={{ x: 100, duration: 500 }}>
+			<button
+				class="h-full w-[30%]"
+				onclick={() => (open = false)}
+				aria-label="close navigation menu"
+			></button>
+			<div class="flex h-full w-[70%] flex-col items-center gap-3 bg-gray-500/90 pt-5">
+				<a href="/" class="text-secondary text-2xl" onclick={() => (open = false)}>Home</a>
+				<a href="/menu" class="text-secondary text-2xl" onclick={() => (open = false)}>Menu</a>
+				<a href="/about" class="text-secondary text-2xl" onclick={() => (open = false)}>About</a>
+			</div>
+		</div>
+	{/if}
 </nav>
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,62 @@
-<script lang='ts'>
-    import files from '$lib/imageData.json';
-    import emblaCarouselSvelte from 'embla-carousel-svelte';
-    import Autoplay from 'embla-carousel-autoplay'
+<script lang="ts">
+	import files from '$lib/imageData.json';
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import Autoplay from 'embla-carousel-autoplay';
 
-    let options = { loop: true };
-    let plugins = [Autoplay()];
+	let options = { loop: true };
+	let plugins = [Autoplay()];
 </script>
 
-<div class="absolute inset-0 bg-[url('/images/grain.png')] bg-repeat opacity-25 pointer-events-none z-0"></div>
+<div
+	class="pointer-events-none absolute inset-0 z-0 bg-[url('/images/grain.png')] bg-repeat opacity-25"
+></div>
 
-<div class='px-10 flex flex-col gap-5 pt-5'>
-    <hr class='text-secondary mx-10'>
-    <section class='flex flex-col gap-5'>
-        <h1 class='mx-auto w-fit text-2xl'>Featured Items</h1>
-        <div class='overflow-hidden h-52 rounded-lg' use:emblaCarouselSvelte="{{ options, plugins }}">
-            <div class="flex">
-                {#each files.images as image}
-                <div class='flex-none w-full h-full'>
-                    <img class='w-full h-full object-cover object-center' src={image.src} alt={image.alt} title={image.title}>
-                </div>
-                {/each}
-            </div>
-        </div>
-    </section>
-    <hr class='text-secondary mx-10'>
-    <section>
-        <h1 class='mx-auto w-fit text-2xl'>Hours</h1>
-        <div class="w-full grid auto-rows-auto grid-cols-2 px-10 text-sm py-2">
-            <div class="flex flex-col gap-2">
-                <span>Mon - Thu</span>
-                <span>Fri - Sun</span>
-            </div>
-            <div class='flex items-end flex-col gap-2'>
-                <span>12pm - 10pm</span>
-                <span>12pm - 11pm</span>
-            </div>
-        </div>
-    </section>
-    <hr class='text-secondary mx-10'>
-    <section>
-        <h1 class='mx-auto w-fit text-2xl'>Location</h1>
-        <div class="flex flex-col justify-center items-center text-sm py-2 gap-2 pb-5">
-            <p class='text-center p-0'>6060 Blundell Richmond, BC V7C 5J9</p>
-            <div class='z-20 rounded-lg overflow-hidden'>
-                <iframe
-                    title='google location'
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5218.793927087424!2d-123.1580072!3d49.155073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54860b316a5231e3%3A0xef31fdb7c92c9071!2zUG9sYXIgQmVhciBTd2VldCBIb3VzZSDljJfmnoHnhornlJzlk4HlsYs!5e0!3m2!1sen!2sca!4v1749168176789!5m2!1sen!2sca"
-                    class="w-full max-w-max border-0"
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </div>
-        </div>
-    </section>
+<div class="flex flex-col gap-5 px-10 pt-5">
+	<section class="flex flex-col gap-5">
+		<h1 class="mx-auto w-fit text-2xl">Featured Items</h1>
+		<div class="h-52 overflow-hidden rounded-lg" use:emblaCarouselSvelte={{ options, plugins }}>
+			<div class="flex">
+				{#each files.images as image}
+					<div class="h-full w-full flex-none">
+						<img
+							class="h-full w-full object-cover object-center"
+							src={image.src}
+							alt={image.alt}
+							title={image.title}
+						/>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</section>
+	<hr class="text-secondary mx-10" />
+	<section>
+		<h1 class="mx-auto w-fit text-2xl">Hours</h1>
+		<div class="grid w-full auto-rows-auto grid-cols-2 px-10 py-2 text-sm">
+			<div class="flex flex-col gap-2">
+				<span>Mon - Thu</span>
+				<span>Fri - Sun</span>
+			</div>
+			<div class="flex flex-col items-end gap-2">
+				<span>12pm - 10pm</span>
+				<span>12pm - 11pm</span>
+			</div>
+		</div>
+	</section>
+	<hr class="text-secondary mx-10" />
+	<section>
+		<h1 class="mx-auto w-fit text-2xl">Location</h1>
+		<div class="flex flex-col items-center justify-center gap-2 py-2 pb-5 text-sm">
+			<p class="p-0 text-center">6060 Blundell Richmond, BC V7C 5J9</p>
+			<div class="z-20 overflow-hidden rounded-lg">
+				<iframe
+					title="google location"
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5218.793927087424!2d-123.1580072!3d49.155073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54860b316a5231e3%3A0xef31fdb7c92c9071!2zUG9sYXIgQmVhciBTd2VldCBIb3VzZSDljJfmnoHnhornlJzlk4HlsYs!5e0!3m2!1sen!2sca!4v1749168176789!5m2!1sen!2sca"
+					class="w-full max-w-max border-0"
+					loading="lazy"
+					referrerpolicy="no-referrer-when-downgrade"
+				></iframe>
+			</div>
+		</div>
+	</section>
 </div>
