ce7b6d06eba98633d1bc3f458413dba8bb612c09
sxht <31171951+sxht@users.noreply.github.com>
2025-06-07 13:07:17 -0700

    fixed navbar appearing behind page elements

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
     import { fly } from 'svelte/transition';
     let open = $state(false);
-
-    
-    const closeMenu = () => {
-        open = false;
-    }
 </script>
 
-<nav class="w-full relative">
+<nav class="sticky top-0 left-0 w-full z-50 bg-secondary">
   <div class="flex items-center justify-between w-full px-3">
     <img class="w-[20vw] h-auto" src="/images/logo.png" alt="Logo" />
     <div class="absolute left-1/2 -translate-x-1/2">
@@ -20,17 +15,14 @@
   </div>
 
   {#if open}
-    <div
-      class="flex flex-row navigation absolute h-full w-full top-0 left-0"
-      transition:fly={{ x: 100, duration: 500 }}
-    >
+    <div class="flex flex-row navigation absolute h-full w-full top-0 left-0" transition:fly={{ x: 100, duration: 500 }}>
       <button
-        class="w-[30%]"
+        class="w-[30%] h-[100vh]"
         onclick={() => (open = !open)}
         aria-label="close navigation menu"
       ></button>
       <div
-        class="flex flex-col items-center pt-5 gap-3 bg-gray-500/90 w-[70%]"
+        class="flex flex-col items-center pt-5 gap-3 bg-gray-500/90 w-[70%] h-[100vh]"
       >
         <a href="/" class="text-secondary">Home</a>
         <a href="/menu" class="text-secondary">Menu</a>
