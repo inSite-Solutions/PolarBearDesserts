4eedd41b91cb9698225fbdad5e89eb4348b0dd06
sxht <31171951+sxht@users.noreply.github.com>
2025-06-07 16:07:22 -0700

    navbar: added shadow and opacity change on scroll

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import { fly } from 'svelte/transition';
+    import { scrollY } from 'svelte/reactivity/window';
+
     let open = $state(false);
 </script>
 
-<nav class="sticky top-0 left-0 w-full z-50 bg-secondary">
+<!-- you can ignore the scrollY undefined error -->
+<nav class={`sticky bg-secondary top-0 left-0 w-full z-50 transition-all duration-300 
+${scrollY.current > 50 ? 'opacity-80 shadow-lg' : 'opacity-100'}
+`}>
   <div class="flex items-center justify-between w-full px-3">
     <img class="w-[20vw] h-auto" src="/images/logo.png" alt="Logo" />
     <div class="absolute left-1/2 -translate-x-1/2">
@@ -24,9 +29,9 @@
       <div
         class="flex flex-col items-center pt-5 gap-3 bg-gray-500/90 w-[70%] h-[100vh]"
       >
-        <a href="/" class="text-secondary" onclick={() => (open = false)}>Home</a>
-        <a href="/menu" class="text-secondary" onclick={() => (open = false)}>Menu</a>
-        <a href="/about" class="text-secondary" onclick={() => (open = false)}>About</a>
+        <a href="/" class="text-secondary text-2xl" onclick={() => (open = false)}>Home</a>
+        <a href="/menu" class="text-secondary text-2xl" onclick={() => (open = false)}>Menu</a>
+        <a href="/about" class="text-secondary text-2xl" onclick={() => (open = false)}>About</a>
       </div>
     </div>
   {/if}
